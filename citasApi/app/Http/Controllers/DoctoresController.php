80276154215b685491079f32bb654827fb649329
carlos <carlosjuliantarazonaperez@gmail.com>
2025-08-20 23:24:57 -0500

    Doctores |- archivo DoctoresController.php completamente terminado |- endpoints creados y testeados, funcionan correctamente

--- citasApi/app/Http/Controllers/DoctoresController.php
+++ citasApi/app/Http/Controllers/DoctoresController.php
@@ -2,9 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctores;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DoctoresController extends Controller
 {
-    //
+    public function index()
+    {
+        $doctores = Doctores::all();
+        return response()->json($doctores);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required|string|max:255',
+            'apellido' => 'required|string|max:255',
+            'especialidad' => 'required|string|max:255',
+            'horario' => 'required|string|max:255',
+            'lugar_trabajo' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $doctor = Doctores::create($validator->validated());
+        return response()->json($doctor, 201);
+    }
+
+    public function show($id)
+    {
+        $doctor = Doctores::find($id);
+        if (!$doctor) {
+            return response()->json(['message' => 'Doctor no encontrado'], 404);
+        }
+        return response()->json($doctor);
+    }
+
+    public function  update(Request $request, string $id)
+    {
+        $doctor = Doctores::find($id);
+        if (!$doctor) {
+            return response()->json(['message' => 'Doctor no encontrado'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'string|max:255',
+            'apellido' => 'string|max:255',
+            'especialidad' => 'string|max:255',
+            'horario' => 'string|max:255',
+            'lugar_trabajo' => 'string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $doctor->update($validator->validated());
+
+        return  response()->json($doctor);
+    }
+
+    public function destroy(string $id)
+    {
+        $doctor = Doctores::find($id);
+        if (!$doctor) {
+            return response()->json(['message' => 'Doctor no encontrado'], 404);
+        }
+        $doctor->delete();
+        return response()->json(['message' => 'Doctor eliminado con exito']);
+    }
 }
--- citasApi/routes/api.php
+++ citasApi/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PacientesController;
+use App\Http\Controllers\DoctoresController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,11 @@ Route::get('paciente/{id}', [PacientesController::class, 'show']);
 Route::post('crearPaciente', [PacientesController::class, 'store']);
 Route::put('actualizarPaciente/{id}', [PacientesController::class, 'update']);
 Route::delete('eliminarPaciente/{id}', [PacientesController::class, 'destroy']);
+
+Route::get('listarDoctores', [DoctoresController::class, 'index']);
+Route::get('doctor/{id}', [DoctoresController::class, 'show']);
+Route::post('crearDoctor', [DoctoresController::class, 'store']);
+Route::put('actualizarDoctor/{id}', [DoctoresController::class, 'update']);
+Route::delete('eliminarDoctor/{id}', [DoctoresController::class, 'destroy']);
+
+
